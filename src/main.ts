2c2d246569d0faed4fffedebc635740901a4392a
Anmol Sethi <anmol@terrastruct.com>
2022-12-13 01:21:20 -0800

    fmt

--- src/main.ts
+++ src/main.ts
@@ -5,36 +5,32 @@ import { D2Processor } from "./processor";
 import { RecompileIcon } from "./constants";
 
 export default class D2Plugin extends Plugin {
-	settings: D2PluginSettings;
-	processor: D2Processor;
+  settings: D2PluginSettings;
+  processor: D2Processor;
 
-	async onload() {
-		addIcon("recompile", RecompileIcon);
-		await this.loadSettings();
-		this.addSettingTab(new D2SettingsTab(this.app, this));
+  async onload() {
+    addIcon("recompile", RecompileIcon);
+    await this.loadSettings();
+    this.addSettingTab(new D2SettingsTab(this.app, this));
 
-		const processor = new D2Processor(this);
-		this.registerMarkdownCodeBlockProcessor("d2", processor.attemptExport);
+    const processor = new D2Processor(this);
+    this.registerMarkdownCodeBlockProcessor("d2", processor.attemptExport);
 
-		this.processor = processor;
-	}
+    this.processor = processor;
+  }
 
-	onunload() {
-		const abortControllers = this.processor.abortControllerMap.values();
-		Array.from(abortControllers).forEach((controller) => {
-			controller.abort();
-		});
-	}
+  onunload() {
+    const abortControllers = this.processor.abortControllerMap.values();
+    Array.from(abortControllers).forEach((controller) => {
+      controller.abort();
+    });
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData()
-		);
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
